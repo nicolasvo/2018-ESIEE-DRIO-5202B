fed8633d2ac8a957ae29639cfb8df59b0d00fb7b
Nicolas Vo <32842375+nicolasvo@users.noreply.github.com>
2018-11-05 11:31:49 +0100

    Update Complex.scala
    hehehe

--- labs/01/src/main/scala/edu/esiee/scala18/Complex.scala
+++ labs/01/src/main/scala/edu/esiee/scala18/Complex.scala
@@ -5,5 +5,12 @@ object Complex {
 }
 
 class Complex(val re: Double, val im: Double) {
+
+  def + (b: Complex) = Complex(this.re + b.re, this.im + b.im)
+  def - (b: Complex) = Complex(this.re - b.re, this.im - b.im)
+  def * (b: Complex) = Complex(this.re * b.re - (this.im*b.im), this.re*b.im + this.im*b.re)
+  def unary_- () = Complex(-this.re, -this.im)
+  def % (m: int) = Complex()
+  //def / (b: Complex)
   override def toString = s"$re+i$im"
 }
